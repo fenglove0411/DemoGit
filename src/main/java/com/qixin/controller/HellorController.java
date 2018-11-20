7e06728c2daa8caabac2e09e510d75f0ed1f170a
fengyunpeng <83779812@email.com>
2018-11-20 09:35:09 +0800

    这是dgit项目的修改，增加了一个dgit方法，用来区分项目

--- src/main/java/com/qixin/controller/HellorController.java
+++ src/main/java/com/qixin/controller/HellorController.java
@@ -3,6 +3,10 @@ package com.qixin.controller;
 public class HellorController {
 	public void hello() {
 		System.out.println("hello");
+		System.out.println("DemoGit");
+	}
+	public void dGit() {
+		System.out.println("dGit");
 	}
 
 }
